2a729ce877f237dac7f53dc64ac4c1cc3a1fc72c
Robert Wiggins <wigginsro11@gmail.com>
2019-08-02 12:03:37 -0400

    fixed package json breaking error, renders

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+
+  state = {
+    temp: '',
+  }
+
+  render() {
+    return <div className="App">I can render</div>;
+  }
+}
+
+export default App;
